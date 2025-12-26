367248c67fe7ca65c9ceb1f24c4818359492472c
Pedro <izielucas2016@gmail.com>
2025-12-26 19:34:53 -0300

    feat: fixed colors

--- src/components/PorQue.vue
+++ src/components/PorQue.vue
@@ -12,7 +12,7 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 <div
                     class="flex flex-col items-center text-center p-6 bg-[#f7f7f7] rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
-                    <div class="text-green-500 text-5xl mb-4">
+                    <div class="text-stone-900 text-5xl mb-4">
                         <i class="fas fa-user-tie"></i>
                     </div>
                     <h3 class="text-xl font-bold mb-2">Profissionais Qualificados</h3>
@@ -22,7 +22,7 @@
                 </div>
                 <div
                     class="flex flex-col items-center text-center p-6 bg-[#f7f7f7] rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
-                    <div class="text-green-500 text-5xl mb-4">
+                    <div class="text-stone-900 text-5xl mb-4">
                         <i class="fas fa-handshake"></i>
                     </div>
                     <h3 class="text-xl font-bold mb-2">Atendimento Personalizado</h3>
@@ -32,7 +32,7 @@
                 </div>
                 <div
                     class="flex flex-col items-center text-center p-6 bg-[#f7f7f7] rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
-                    <div class="text-green-500 text-5xl mb-4">
+                    <div class="text-stone-900 text-5xl mb-4">
                         <i class="fas fa-shield-alt"></i>
                     </div>
                     <h3 class="text-xl font-bold mb-2">Compromisso com a Segurança</h3>
@@ -42,7 +42,7 @@
                 </div>
                 <div
                     class="flex flex-col items-center text-center p-6 bg-[#f7f7f7] rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
-                    <div class="text-green-500 text-5xl mb-4">
+                    <div class="text-stone-900 text-5xl mb-4">
                         <i class="fas fa-map-marked-alt"></i>
                     </div>
                     <h3 class="text-xl font-bold mb-2">Cobertura Ampla</h3>
