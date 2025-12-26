c1107d61bb8b62fafb9578a2b78e09093497d406
Pedro <izielucas2016@gmail.com>
2025-12-26 14:10:59 -0300

    feat: created whatsapp component

---
+++ src/components/Whatsapp.vue
@@ -0,0 +1,15 @@
+<script setup>
+import whatsappIcon from '@/assets/wa-whatsapp-icon.svg'
+const phone = '5582999999999'
+const message = 'Olá! Gostaria de solicitar um orçamento.'
+</script>
+
+<template>
+  <a
+    :href="`https://wa.me/${phone}?text=${encodeURIComponent(message)}`"
+    target="_blank"
+    class="fixed bottom-6 right-6 transition hover:scale-105"
+  >
+    <img :src="whatsappIcon" class="w-full h-[75px]">
+  </a>
+</template>
