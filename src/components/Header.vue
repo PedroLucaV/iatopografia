2ee797080bcb68ee35ad896a9f97dcf0ecb897e1
Pedro <izielucas2016@gmail.com>
2025-12-26 12:39:51 -0300

    feat: created header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="flex items-center justify-around w-full h-[100px] bg-[#222222]">
+        <img src="/logo.png" alt="logo" class="h-full">
+        <ul class="flex text-white font-bold uppercase text-lg gap-[30px]">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </header>
+</template>
