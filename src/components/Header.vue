839b0106c7b3ce8d7735444128df6ce941f7944a
Pedro <izielucas2016@gmail.com>
2025-12-26 13:53:07 -0300

    feat: added new style to link in header and created an array for each link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,17 @@
 <script setup>
-
+const links = ['inicio', 'sobre', 'projetos', 'contato'];
 </script>
 
 <template>
     <header class="flex items-center justify-around w-full h-[100px] bg-[#222222]">
         <img src="/logo.png" alt="logo" class="h-full">
         <ul class="flex text-white font-bold uppercase text-lg gap-[30px]">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Projetos</a></li>
-            <li><a href="#">Contato</a></li>
+            <li v-for="link in links" :key="link">
+               <a
+                 :href="`#${link}`" class="relative transition hover:text-green-400 after:absolute after:left-0 after:-bottom-1 after:h-[2px] :w-0 after:bg-green-400 after:transition-all hover:after:w-full">
+                {{ link }}
+            </a>
+            </li>
         </ul>
     </header>
 </template>
