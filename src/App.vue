112403d317c31111980fa9e74b9e6ab4275ecce2
Pedro <izielucas2016@gmail.com>
2025-12-26 18:45:19 -0300

    feat: app improved

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+  import Galerias from './components/Galerias.vue';
   import Header from './components/Header.vue';
   import Inicio from './components/Inicio.vue';
-import Whatsapp from './components/Whatsapp.vue';
+  import Sobre from './components/Sobre.vue';
+  import Whatsapp from './components/Whatsapp.vue';
 </script>
 
 <template>
   <Header></Header>
   <Inicio></Inicio>
+  <Sobre></Sobre>
+  <Galerias></Galerias>
   <Whatsapp></Whatsapp>
 </template>
 
